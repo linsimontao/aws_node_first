52f8db8d483929976b93fbcbd825de812165cfce
unknown <linsimontao@gmail.com>
2020-11-19 17:24:48 +0900

    change to PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 console.log(port);
 app.listen(port, () => {
     console.log("wazzapp?")
